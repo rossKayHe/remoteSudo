0b6a58814f51e6a6576613204c676421c8f49282
rossKayHe <rossKayHe@users.noreply.github.com>
2019-12-09 08:29:09 -0800

    Update remoteSudo.sh

--- remoteSudo.sh
+++ remoteSudo.sh
@@ -3,13 +3,23 @@ user=$1
 pwd=$2
 cmd=$3
 host=$4
+
 function usage {
   echo "Usage remoteSudo.sh <user> <password> <command> <host file||comma delimited list>"
   exit
 }
+
+# install sshpass if not already ibnstalled
+if [ ! echo $2 | sudo -S yum list sshpass | grep 'Installed Packages' ]; then
+  echo $2 | sudo -S yum install sshpass -yum
+fi
+
+# Check number of arguments
 if  [ "$#" -ne 4 ]; then
   usage
 fi
+
+# Check for comma delimited list or filename
 if [[ $4 == *[,]* ]]; then
   IFS=',' read -r -a hosts <<< "$4"
 elif [[ $4 == *"/"* ]]; then
@@ -17,8 +27,8 @@ elif [[ $4 == *"/"* ]]; then
 else
   usage
 fi
-#printf '%s\n' "${hosts[@]}"
-#sshpass -p $2 ssh  -o "StrictHostKeyChecking no" $1@$3 "echo $2 | sudo -S $4"
+
+# Run remote sudo command on list of hosts
 for h in "${hosts[@]}"
 do
   sshpass -p $2 ssh  -o "StrictHostKeyChecking no" $1@$h "echo $2 | sudo -S $3"
